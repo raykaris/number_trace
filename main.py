50fb702de73be54f0a980e92b57ee805f8d4b923
raykaris <130473179+raykaris@users.noreply.github.com>
2024-03-19 19:13:04 +0300

    Add files via upload
    the location.html shows the latitude and longitude position in the map.

---
+++ main.py
@@ -0,0 +1,25 @@
+import phonenumbers
+import folium
+from phonenumbers import geocoder
+
+key = "2468a63f4bff4f7eb2bbd7c7c29887d4"
+number=input("Enter phone number with country code:")
+check_number = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(check_number,"en")
+print(number_location)
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider,"en"))
+
+from opencage.geocoder import OpenCageGeocode
+geocoder = OpenCageGeocode(key)
+query = str(number_location)
+results = geocoder.geocode(query)
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat , lng)
+
+map_location = folium.Map(location = [lat,lng],zoom_start=9)
+folium.Marker([lat,lng], popup = number_location).add_to(map_location)
+map_location.save("location.html")
